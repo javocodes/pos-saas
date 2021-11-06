aa646e536232aafde8d1f7e858307400f7e8474d
OriginsPD <88017775+OriginsPD@users.noreply.github.com>
2021-11-05 19:36:37 -0500

    Dashboard Update

---
+++ app/Http/Livewire/Storefront/Backend/Adminindox.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire\Storefront\Backend;
+
+use Livewire\Component;
+
+class Adminindox extends Component
+{
+    public function render()
+    {
+        return view('livewire.storefront.backend.adminindox');
+    }
+}
