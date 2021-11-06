d2c006e911974f7e90a9b9bd1ddf21d0cc79c678
OriginsPD <88017775+OriginsPD@users.noreply.github.com>
2021-11-05 19:47:29 -0500

    Components Added

--- resources/views/livewire/storefront/navigation/admin-header.blade.php
+++ resources/views/livewire/storefront/navigation/admin-header.blade.php
@@ -53,7 +53,7 @@
                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-indigo-600 hover:text-white">Profile</a>
                 <a href="#"
                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-indigo-600 hover:text-white">Products</a>
-                <a href="/login"
+                <a href=""
                    class="block px-4 py-2 text-sm text-gray-700 hover:bg-indigo-600 hover:text-white">Logout</a>
             </div>
         </div>
