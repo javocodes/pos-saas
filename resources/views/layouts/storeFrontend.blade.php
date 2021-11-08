8d987bd5a4e898233ec5ef71c24cd70bcb435d35
OriginsPD <88017775+OriginsPD@users.noreply.github.com>
2021-11-08 12:10:38 -0500

    StoreFront Added

---
+++ resources/views/layouts/storeFrontend.blade.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>{{ config('app.name') }}</title>
+    @livewireStyles
+    <link rel="stylesheet" href="{{ mix('css/app.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/scroll.css') }}">
+    <link href="https://kit-pro.fontawesome.com/releases/v5.15.4/css/pro.min.css" rel="stylesheet">
+</head>
+<body>
+
+<div
+    class="relative flex items-top justify-center w-screen bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
+
+    <section class="relative bg-gray-100 overflow-x-hidden">
+
+        @livewire('storefront.navigation.home-navi')
+
+        @yield('content')
+
+        @livewire('storefront.frontend.products')
+
+    </section>
+
+</div>
+
+        @livewireScripts
+        <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+</body>
+</html>
