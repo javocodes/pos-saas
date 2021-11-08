62f489f82b33685e13dc892a480141ab10fb9df6
KG-DI-KXNG <kevandogordon@yahoo.com>
2021-11-08 13:25:59 -0500

    Dashbodard route redirect change

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    public const HOME = \route('backEnd.index');
 
     /**
      * The controller namespace for the application.
