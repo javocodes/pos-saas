16741297163ccebba57ef25c94fe9ccd3e40d509
Rajeime <rajeime.m.roberts@gmail.com>
2021-11-16 23:40:17 -0500

    Created table for associate information, edited livewire to update information with editing functionality ----GRANT

--- app/Http/Livewire/Storefront/Backend/Store/CreateAsscoiate.php
+++ app/Http/Livewire/Storefront/Backend/Store/CreateAsscoiate.php
@@ -18,6 +18,8 @@ class CreateAsscoiate extends Component
     public $password = '';
     public $user_type;
 
+    public $associates;
+
     protected $rules = [
         'first_name' => 'required|max:12',
         'last_name' => 'required|max:12',
@@ -38,6 +40,8 @@ class CreateAsscoiate extends Component
             'password' => Hash::make($this->password),
         ]);
 
+        $this->associates = User::all();
+
         session()->flash('success', 'Sales Associate Added Successfully');
 
 
@@ -48,9 +52,18 @@ class CreateAsscoiate extends Component
         $this->validate();
     }
 
+    function delete($id)
+    {
+
+        User::where('id','=',$id)->delete();
+
+        $this->associates = User::all();
+    }
+
     public function mount()
     {
         $this->password = Str::random(10);
+        $this->associates = User::all();
     }
 
     public function render()
